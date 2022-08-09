74a4e4581bb62d7eadbbda6c5fb3df96d4a6129d
PabloSerra07 <pablojaviej@gmail,com>
2022-08-08 22:28:31 -0300

    eventos

--- JS/entrega.js
+++ JS/entrega.js
@@ -25,84 +25,123 @@ const juego4 = new nombreJuegos (4,"Islander","Aventura",1987, "./img/Islander.j
 
 const juegos = [juego1,juego2,juego3,juego4] 
 
-// juegos.forEach((game)=>game.mostrarJuegos())
+juegos.forEach((game)=>game.mostrarJuegos())
 
+let tarjetaNueva = document.getElementById("juegos")
+tarjetaNueva.setAttribute("class","estiloTarjetas")
+function verCatalogo(){
 
-// ///////// Ingresar Juegos nuevos ///////////////////
-// function nuevoJuego(){
-//     let juegoIngresado = prompt("Ingrese el Juego")
-//     let generoIngresado = prompt("Ingrese el genero")
-//     let añoIngresado = parseInt(prompt("Ingrese el año"))
-//     let precioIngresado = parseInt(prompt("Ingrese el precio"))
-//     let juegoCreado = new nombreJuegos(juegos.length+1,juegoIngresado, generoIngresado, añoIngresado, precioIngresado)
-//     console.log(juegoCreado)
-//     juegos.push(juegoCreado)
-// }
-// /////Buscar juego por nombre//////////
-// function busquedaPorNombre(){
-//     let ingNombre = prompt(`Ingrese el Nombre del Juego`)
-//     let busqueda = juegos.filter((juego)=>juego.nombre == ingNombre)
-//     console.log(busqueda)
-//     if(busqueda == false){
-//         console.log(`Su Juego NO se encuentra en Nuestro Catalogo!!`)
-//     }
-// }
-
-// ///////////Catalogo////////////
-// function verCatalogo(){
-//     alert(`Catalogo de Juegos: "Ver en Consola"`)
-//     for(let game of juegos){
-//         game.mostrarJuegos()
-//     }
-// }
-
-// /////////Menu//////////
-// function preguntarOpciones(){
-//     let opcion = parseInt(prompt(`Opciónes:
-//                         1 - Ver catálogo de Juegos
-//                         2 - Agregar Juego 
-//                         3 - Buscar por nombre:
-//                         0 - Para salir
-//                         `))
-//     menu(opcion)
-// }    
-// function menu(opcionSeleccionada){
-//     switch(opcionSeleccionada){
-//         case 0:
-//             salir = true
-//             alert(`Gracias, por visitar nuestera plataforma Gamer!!!`)
-//         break    
-//         case 1:
-//             verCatalogo()
-
-//       	break 
-//    	    case 2: 
-//             nuevoJuego()
-//       	break 
-//    	    case 3: 
-//             busquedaPorNombre()
-
-//       	break 
-//    	    default: 
-//       	alert(`Ingrese una opción correcta`)
-//     }
-// }
-// preguntarOpciones()
-
-///////------Plantilla-------////////
-
-// let divJuegos = document.getElementById("juegos")
-
-juegos.forEach((juegos)=>{ 
-    let juegoNuevo = document.createElement("div")
-    juegoNuevo.innerHTML = `<section id="${juegos.id}" class="card">
-                                <h2 class="tituloCard">${juegos.nombre}</h2>
-                                <img class="imgCard" src="${juegos.imagen}" alt="${juegos.titulo}">
-                                <div class="info">
-                                    <p class="generoCard">${juegos.genero}</p>
-                                    <p id="anioCard" class="anioCard">${juegos.año}</p>
-                                </div>
+    juegos.forEach((juego) =>{
+let nuevoJuego = document.createElement("div")
+    nuevoJuego.innerHTML = `<section id="${juego.id}" class="card">
+                                    <h2 class="tituloCard">${juego.nombre}</h2>
+                                    <img class="imgCard" src="${juego.imagen}" alt="${juego.titulo}">
+                                    <div class="info">
+                                        <p class="generoCard">${juego.genero}</p>
+                                        <p id="anioCard" class="anioCard">${juego.año}</p>
+                                    </div>
                             </section>`
-    
-    document.body.append(juegoNuevo) 
+tarjetaNueva.appendChild(nuevoJuego)
 })
+}
+
+/////Buscar juego por nombre//////////
+function busquedaPorNombre(){
+    let ingNombre = prompt(`Ingrese el Nombre del Juego`)
+    let busqueda = juegos.filter((juego)=>juego.nombre == ingNombre)
+    console.log(busqueda)
+    if(busqueda == false){
+        console.log(`Su Juego NO se encuentra en Nuestro Catalogo!!`)
+    }
+}
+
+
+/////---------Imput-----------///////
+function guardarJuego(){
+    let nombreInput = document.getElementById("nombreInput")
+    let generoInput = document.getElementById("generoInput")
+    let añoInput = document.getElementById("añoInput")
+    let imageInput = document.getElementById("imageInput")
+    let juegoCreado = new nombreJuegos(juegos.length+1, nombreInput.value, generoInput.value, añoInput.value, "img/Super Mario.jpg" )
+    juegos.push(juegoCreado)
+    verCatalogo()
+    
+}
+
+//////--------------Boton Agregar--------//////////
+const guardarJuegoBtn = document.getElementById("guardarJuegoBtn")
+guardarJuegoBtn.addEventListener("click", guardarJuego)
+
+///////-----catalogo-----///////////
+let verCatalgoBtn = document.getElementById("verCatalgoBtn")
+verCatalgoBtn.addEventListener("click", verCatalogo)
+
+/////////Menu//////////
+// function preguntarOpciones(){
+    //     let opcion = parseInt(prompt(`Opciónes:
+    //                         1 - Ver catálogo de Juegos
+    //                         2 - Agregar Juego 
+    //                         3 - Buscar por nombre:
+    //                         0 - Para salir
+    //                         `))
+    //     menu(opcion)
+    // }    
+    // function menu(opcionSeleccionada){
+        //     switch(opcionSeleccionada){
+            //         case 0:
+            //             salir = true
+            //             alert(`Gracias, por visitar nuestera plataforma Gamer!!!`)
+            //         break    
+            //         case 1:
+            //             verCatalogo()
+            
+            //       	break 
+            //    	    case 2: 
+            //             nuevoJuego()
+            //       	break 
+            //    	    case 3: 
+            //             busquedaPorNombre()
+            
+            //       	break 
+            //    	    default: 
+            //       	alert(`Ingrese una opción correcta`)
+            //     }
+            // }
+            // preguntarOpciones()
+            
+            ///////------Plantilla-------////////
+            
+            // let divJuegos = document.getElementById("juegos")
+            
+            // juegos.forEach((juegos)=>{ 
+                //     let juegoNuevo = document.createElement("div")
+                //     juegoNuevo.innerHTML = `<section id="${juegos.id}" class="card">
+                //                                 <h2 class="tituloCard">${juegos.nombre}</h2>
+                //                                 <img class="imgCard" src="${juegos.imagen}" alt="${juegos.titulo}">
+                //                                 <div class="info">
+                //                                     <p class="generoCard">${juegos.genero}</p>
+                //                                     <p id="anioCard" class="anioCard">${juegos.año}</p>
+                //                                 </div>
+                //                             </section>`
+                
+                //     document.body.append(juegoNuevo) 
+                // })
+                
+                ///////////Catalogo////////////
+                // function verCatalogo(){
+                    //         for(let game of juegos){
+                        //         game.mostrarJuegos()
+                        //     }
+                        // }
+                        
+                        ///////// Ingresar Juegos nuevos ///////////////////
+                        // function nuevoJuego(){
+                        //     let juegoIngresado = prompt("Ingrese el Juego")
+                        //     let generoIngresado = prompt("Ingrese el genero")
+                        //     let añoIngresado = parseInt(prompt("Ingrese el año"))
+                        //     let juegoCreado = new nombreJuegos(juegos.length+1,juegoIngresado, generoIngresado, añoIngresado)
+                        //     console.log(juegoCreado)
+                        //     juegos.push(juegoCreado)
+                        // }
+                
+                
